29626f444eacf4aa9300b3f50279d7a1dad10c02
bhargavi539 <48649370+bhargavi539@users.noreply.github.com>
2024-08-17 22:12:27 +0200

    Folder structure changed and operators, literals concepts

---
+++ src/python_coding_tasks/2024-08-17/task12.py
@@ -0,0 +1,28 @@
+#int, float, bool,complex, str
+#list, set, frozenset, tuple, noneType
+
+#variable name = value
+#identifier =(operator) literal
+
+name = "Frederick"
+
+my_list =[10,20,30,40]
+
+my_tuple =("bread","pasta","milk")
+
+complex_number = 1+7j
+
+#escape sequence
+
+print("Hello World")
+print("Hello\nWorld") #\n -> new line
+print("Hello\tWorld") #\t -> tabline
+print("Hello\bWorld") #\b -> backspace
+
+#dir =´D:\user\n.txt´
+#dir ="D:\user\n.txt"
+
+#this concept is used in automation
+#r -> refers to raw data
+dir =r"D:\user\n.txt"
+print(dir)
---
+++ src/python_coding_tasks/2024-08-17/task_13.py
@@ -0,0 +1,34 @@
+#unary operator
+do_i_have_a_bicycle = -1
+
+#x//y --> gives quotient
+#x**y --> gives power of a number
+#x%y --> gives reminder
+
+#operators
+#not operator
+
+user_checked_in = True
+print(not user_checked_in)
+
+#logical operators
+#< , > ,<= ,>=
+
+x = 10
+y = 20
+print(x>=y)
+print(x<=y)
+
+#Ternary operator
+
+# if condition do this
+#    else do this
+
+my_age = 15
+print("Can go to Goa" if my_age > 18 else "Can´t go")
+
+if(my_age > 18):
+    print("Can go to Goa")
+else:
+    print("can´t go")
+
