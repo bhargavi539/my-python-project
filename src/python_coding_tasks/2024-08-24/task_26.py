e2940129540f336c4c4cfc606de9a6b5b646e52b
bhargavi539 <48649370+bhargavi539@users.noreply.github.com>
2024-08-26 22:27:41 +0200

    Arguments list *args

--- src/python_coding_tasks/2024-08-24/task_26.py
+++ src/python_coding_tasks/2024-08-24/task_26.py
@@ -1,12 +1,15 @@
 #Sum of three numbers with input from the user
-number1 = int(input("Enter number1 :"))
-number2 = int(input("Enter number2 :"))
-number3 = int(input("Enter number3 :"))
+#number1 = int(input("Enter number1 :"))
+#number2 = int(input("Enter number2 :"))
+#number3 = int(input("Enter number3 :"))
 
 
-def sum_of_three_numbers(n1, n2, n3):
+def sum_of_three_numbers(n1=10, n2=20, n3=15):
     return n1 + n2 + n3
 
 
-result = sum_of_three_numbers(number1, number2, number3)
-print(result)
+#result = sum_of_three_numbers(number1, number2, number3)
+result1 = sum_of_three_numbers(10,20,30)
+result2 = sum_of_three_numbers(n1=40,n3=40)
+result3 = sum_of_three_numbers(n2=50)
+print(result3)
---
+++ src/python_coding_tasks/2024-08-24/task_27.py
@@ -0,0 +1,7 @@
+def print_arguments(*args):
+    print(*args)
+
+
+print_arguments(20,30,40)
+print_arguments("Sandy",30,"Anudeep")
+print_arguments("Sandy",30,27.45,True)
